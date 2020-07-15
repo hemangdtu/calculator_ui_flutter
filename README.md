f8343afd597c979050472ff78cb57face9d0668f
hemangdtu <hemangdtu@gmail.com>
2020-07-15 23:11:14 +0530

    input, clear and delete activated

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
+<<<<<<< HEAD
 # calculator_ui_flutter
 Flutter is an open-source SDK, by Google, used for creating high-performance, high-fidelity mobile apps for iOS and Android. The Flutter framework makes it easy for you to build user interfaces that react smoothly in your app, while reducing the amount of code required to synchronize and update your app’s view. Flutter makes it easy to build beautiful apps with its rich set of Material Design widgets and behaviours. Here I have tried building a simple calculator with an attractive UI in Flutter.
+=======
+<<<<<<< HEAD
+# calculator_ui_flutter
+Flutter is an open-source SDK, by Google, used for creating high-performance, high-fidelity mobile apps for iOS and Android. The Flutter framework makes it easy for you to build user interfaces that react smoothly in your app, while reducing the amount of code required to synchronize and update your app’s view. Flutter makes it easy to build beautiful apps with its rich set of Material Design widgets and behaviours. Here I have tried building a simple calculator with an attractive UI in Flutter.
+=======
+# gdg_app
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+>>>>>>> UI Design ready
+>>>>>>> UI Design complete
---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:gdg_app/buttons.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var userQuestion = '';
+  var userAnswer = '';
+  final myTextStyle = TextStyle(
+    fontSize: 30,
+    color: Colors.deepPurple[900],
+  );
+  final List<String> buttons = [
+    "C",
+    "DEL",
+    "%",
+    "/",
+    "9",
+    "8",
+    "7",
+    "*",
+    "6",
+    "5",
+    "4",
+    "-",
+    "3",
+    "2",
+    "1",
+    "+",
+    ".",
+    "0",
+    "ANS",
+    "=",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      userQuestion,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      userAnswer,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Container(
+              child: GridView.builder(
+                itemCount: buttons.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  if (index == 0) {
+                    return MyButton(
+                      buttonTapped: () {
+                        setState(() {
+                          userQuestion = '';
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: Colors.green,
+                      textColor: Colors.white,
+                    );
+                  } else if (index == 1) {
+                    return MyButton(
+                      buttonTapped: () {
+                        setState(() {
+                          userQuestion = userQuestion.substring(
+                              0, userQuestion.length - 1);
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: Colors.red,
+                      textColor: Colors.white,
+                    );
+                  } else {
+                    return MyButton(
+                      buttonTapped: () {
+                        setState(() {
+                          userQuestion += buttons[index];
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: isOperator(buttons[index])
+                          ? Colors.deepPurple
+                          : Colors.deepPurple[50],
+                      textColor: isOperator(buttons[index])
+                          ? Colors.white
+                          : Colors.deepPurple,
+                    );
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  bool isOperator(String x) {
+    if (x == "%" || x == "/" || x == "*" || x == "-" || x == "+" || x == "=")
+      return true;
+    return false;
+  }
+}
